8fc0188225f738aa4493fe41698a87deeaa67a14
Luciano Terres <lucianoterres@gmail.com>
2023-02-03 18:13:19 -0300

    seismic Dataset

---
+++ constDirectories.py
@@ -0,0 +1,12 @@
+dataSetDIR = f"D:\_0Luciano\_0PHD\datasets"
+masksDir =  f'D:\_0Luciano\_0PHD\datasets\masks10-90'
+
+
+TRAIN_MASK_DIR = './train/'
+TRAIN_CSV = f'D:/_0Luciano/_0PHD/datasets/tgs-salt/train1090.csv'    #./train/train.csv'
+DEPTH_CSV = 'depths.csv'
+
+TRAIN_DIR  = 'D:/_0Luciano/_0PHD/datasets/tgs-salt/'
+TRAIN_IMAGE_DIR = 'D:/_0Luciano/_0PHD/datasets/tgs-salt/train/images/'
+TRAIN_MASK_DIR  = 'D:/_0Luciano/_0PHD/datasets/tgs-salt/train/masks/'
+TEST_IMAGE_DIR  = 'D:/_0Luciano_0PHD/datasets/tgs-salt/test/images/'
