8a44521788319491161b63c684932c42ca71e619
Luciano <lucianoterres@gmail.com>
2023-02-24 20:47:19 -0300

    transporting

--- constDirectories.py
+++ constDirectories.py
@@ -1,12 +1,12 @@
-dataSetDIR = f"D:\_0Luciano\_0PHD\datasets"
-masksDir =  f'D:\_0Luciano\_0PHD\datasets\masks10-90'
+dataSetDIR = f"E:\Luciano\_0PH\Datasets"
+masksDir =  f'E:\Luciano\_0PH\Datasets\masks10-90' #f'D:\_0Luciano\_0PHD\datasets\masks10-90'
 
 
 TRAIN_MASK_DIR = './train/'
-TRAIN_CSV = f'D:/_0Luciano/_0PHD/datasets/tgs-salt/train1090.csv'    #./train/train.csv'
+TRAIN_CSV = f'E:\Luciano\_0PH\Datasets/tgs-salt/train1090.csv'    #./train/train.csv'
 DEPTH_CSV = 'depths.csv'
 
-TRAIN_DIR  = 'D:/_0Luciano/_0PHD/datasets/tgs-salt/'
-TRAIN_IMAGE_DIR = 'D:/_0Luciano/_0PHD/datasets/tgs-salt/train/images/'
-TRAIN_MASK_DIR  = 'D:/_0Luciano/_0PHD/datasets/tgs-salt/train/masks/'
-TEST_IMAGE_DIR  = 'D:/_0Luciano_0PHD/datasets/tgs-salt/test/images/'
+TRAIN_DIR  = 'E:\Luciano\_0PH\Datasets/tgs-salt/'
+TRAIN_IMAGE_DIR = 'E:\Luciano\_0PH\Datasets/tgs-salt/train/images/'
+TRAIN_MASK_DIR  = 'E:\Luciano\_0PH\Datasets/tgs-salt/train/masks/'
+TEST_IMAGE_DIR  = 'E:\Luciano\_0PH\Datasets/tgs-salt/test/images/'
